62f04d3275acee196ea1b0c4e9a217ff895d5a2d
Rohit Gaikwad <rohitrajg9881@gmail.com>
2026-01-10 19:55:42 +0530

    Sync joke and emoji updates using Promise.all

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-function getData ()
-{
-    fetch(`https://official-joke-api.appspot.com/random_joke`)
-        .then((res) => res.json())
-        .then((data) =>
-        {
-            document.getElementById('setup').textContent = data.setup;
-            document.getElementById('punchline').textContent = data.punchline;
-        })
-        .catch((err)=>console.log(err))
-}
-getData()
-setInterval(getData,10000)
-
-function getEmoji() {
-    fetch("https://emojihub.yurace.pro/api/random")
-        .then((res) => res.json())
-        .then((data) => {
-            document.getElementById('emoji').innerHTML = data.htmlCode[0];
-        })
-        .catch((err) => console.log(err));
+function updateJokeAndEmoji() {
+    Promise.all([
+        fetch("https://official-joke-api.appspot.com/random_joke").then(res => res.json()),
+        fetch("https://emojihub.yurace.pro/api/random").then(res => res.json())
+    ])
+    .then(([joke, emoji]) => {
+        document.getElementById("setup").textContent = joke.setup;
+        document.getElementById("punchline").textContent = joke.punchline;
+        document.getElementById("emoji").innerHTML = emoji.htmlCode[0];
+    })
+    .catch(err => console.log(err));
 }
 
-getEmoji();
-setInterval(getEmoji, 10000);
+updateJokeAndEmoji();
+setInterval(updateJokeAndEmoji, 10000);
