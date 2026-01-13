6972c7898d26332cb4a538a69953eeed646455a1
Rohit Gaikwad <rohitrajg9881@gmail.com>
2026-01-13 16:57:50 +0530

    Enhance UI responsiveness and add joke categories

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function updateJokeAndEmoji() {
         fetch("https://emojihub.yurace.pro/api/random").then(res => res.json())
     ])
     .then(([joke, emoji]) => {
+        document.getElementById("heading").textContent = `${joke.type} Humor`;
         document.getElementById("setup").textContent = joke.setup;
         document.getElementById("punchline").textContent = joke.punchline;
         document.getElementById("emoji").innerHTML = emoji.htmlCode[0];
